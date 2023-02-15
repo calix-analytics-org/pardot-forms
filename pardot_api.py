3fc34a32dbc9b43785c6c1586446039663de74bf
mcupo-calix <maile.cupo@calix.com>
2023-02-15 11:13:23 -0800

    credentials bug fixed

--- pardot_api.py
+++ pardot_api.py
@@ -11,7 +11,7 @@ class Pardot():
         host = 'login.salesforce.com'
         endpoint = '/services/oauth2/token'
         business_unit = credentials['business_unit']
-        combined_pw_token = f"{creds['password']}{creds['security_token']}" #Used below as the password param for auth to salesforce
+        combined_pw_token = f"{credentials['password']}{credentials['security_token']}" #Used below as the password param for auth to salesforce
        
         #  Get access token
         resp = self.s.post(f"https://{host}{endpoint}?grant_type={credentials['grant_type']}&client_id={credentials['client_id']}&client_secret={credentials['client_secret']}&username={credentials['username']}&password={combined_pw_token}")
